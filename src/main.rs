bc4f4a7d855bf3fb1767ccea0e3bfd8ca0b7ff56
Fredrik Sommar <fsommar@kth.se>
2015-02-12 16:37:52 -0500

    (feat) added on_err for errors to map to IronResult It might need to be extended in the future but for now its purpose is to map from one type of error which might be out of crate to an IronResult and provide an iron::Response action (e.g. change body, headers or status).

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,51 @@
+#![feature(core)]
 extern crate iron;
+extern crate "error" as err;
 extern crate "rustc-serialize" as rustc_serialize;
 extern crate postgres;
 
+use std::error::{self, Error};
+use std::fmt;
 use postgres::{Connection, SslMode};
-
 use rustc_serialize::{json, Encodable};
 use iron::prelude::*;
 use iron::status;
 use iron::headers;
 
 pub struct Json<T: Encodable>(T);
+pub enum PostgresError {
+    Postgres(postgres::ConnectError)
+}
+
+impl Error for PostgresError {
+    fn description(&self) -> &str {
+        match *self {
+            PostgresError::Postgres(ref err) => err.description()
+        }
+    }
+}
+
+impl fmt::Display for PostgresError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        self.description().fmt(f)
+    }
+}
+
+impl error::FromError<postgres::ConnectError> for Box<PostgresError> {
+    fn from_error(err: postgres::ConnectError) -> Box<PostgresError> {
+        Box::new(PostgresError::Postgres(err))
+    }
+}
+
+trait OnError<T> {
+    fn on_err<M: iron::modifier::Modifier<Response>>(self, m: M) -> IronResult<T>;
+}
+
+impl<T, E: err::Error> OnError<T> for Result<T, E> {
+    fn on_err<M: iron::modifier::Modifier<Response>>(self, m: M) -> IronResult<T> {
+        self.map_err(|x| iron::IronError::new(x, m))
+    }
+}
 
 #[derive(RustcEncodable, RustcDecodable)]
 struct Person {
@@ -22,9 +58,12 @@ fn main() {
         // FIXME: Less unwraps
         // TODO: Don't create a connection for every request?
         // Forward slashes need to be escaped as %2F to be a valid URI
-        let conn = Connection::connect(
+        // FIXME: Remove useless test
+        let err: Result<u32, postgres::ConnectError> = Err(postgres::ConnectError::MissingPassword);
+        try!(err.on_err("Error!"));
+        let conn = try!(Connection::connect(
             "postgresql://postgres@%2Fvar%2Frun%2Fpostgresql",
-            &SslMode::None).unwrap();
+            &SslMode::None).on_err(status::InternalServerError));
 
         let stmt = conn.prepare("SELECT * FROM Person").unwrap();
         let res = stmt.query(&[]).unwrap().map(|x| {
